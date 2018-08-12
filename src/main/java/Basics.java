3db8f35f03f9376840bfc2406640a2a2d3b48236
Navin Tirumal Kotamraju <navin.kotamraju@yash.com>
2018-08-12 21:47:00 +0530

    aug12

---
+++ src/main/java/Basics.java
@@ -0,0 +1,19 @@
+
+public class Basics {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Basics t = new Basics();
+		
+		String str = "naveen";
+		StringBuilder sb = new StringBuilder(str);
+		
+	
+		System.out.println(sb.append("programmer"));
+		
+		
+		
+	}
+
+}
